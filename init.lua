63bdc7874d74245dcde305455c50363d5ce4b345
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2024-05-07 16:23:10 +0200

    Use vis:pipe instead of io.popen for commands

--- init.lua
+++ init.lua
@@ -1,48 +1,37 @@
 local state = {}
 
-local get_cmd = function(cmd)
-  local fz = io.popen(cmd)
-  if fz then
-    local out = fz:read('*a')
-    local _, _, statusop = fz:close()
-    if status == 0 then
-      return out:gsub('\n$', '')
-    end
-  end
-  return nil
-end
-
 vis.events.subscribe(vis.events.FILE_OPEN, function(file)
   if not file.path then
     return
   end
   local escaped = file.path:gsub("'", "'\\''")
+  local status, out, err = vis:pipe(file, { start = 0, finish = 0 }, [[
+    dir="$(dirname ']] .. escaped .. [[')"
+    [ -d "$dir" ] || exit 1
+    if [ -e ']] .. escaped .. [[' ]; then
+      [ -w ']] .. escaped .. [[' ]
+    else
+      [ -w "$dir" ]
+    fi
+  ]])
   state[file.path] = {
-    writable = 'true' == get_cmd([[
-      dir="$(dirname ']] .. escaped .. [[')"
-      [ -d "$dir" ] || exit 1
-      if [ -e ']] .. escaped .. [[' ]; then
-        [ -w ']] .. escaped .. [[' ] && echo "true"
-      else
-        [ -w "$dir" ] && echo "true"
-      fi
-    ]]),
-    escaped = escaped,
+    writable = status == 0,
   }
 end)
 
 vis.events.subscribe(vis.events.FILE_SAVE_PRE, function(file)
   if state[file.path] and not state[file.path].writable then
-    state[file.path].owner = get_cmd([[
+    local status, out, err = vis:pipe(file, { start = 0, finish = 0 }, [[
       ls -l ']] .. state[file.path].escaped .. [[' | cut -d' ' -f4
       sudo chown "$(whoami)" ']] .. state[file.path].escaped .. [[' >/dev/null
     ]])
+    state[file.path].owner = out:gsub('\n$', '')
   end
 end)
 
 vis.events.subscribe(vis.events.FILE_SAVE_POST, function(file)
   if state[file.path] and not state[file.path].writable then
-    get_cmd(
+    local status, out, err = vis:pipe(file, { start = 0, finish = 0 },
       "sudo chown '"
         .. state[file.path].owner:gsub("'", "'\\''")
         .. "' '"
