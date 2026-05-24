4911a46f1e7994a579b171bc4ef1d9c32445f703
Michiel van den Heuvel <michielvdnheuvel@gmail.com>
2026-05-24 21:16:28 +0000

    Fix parsing ls output

--- init.lua
+++ init.lua
@@ -96,7 +96,7 @@ vis.events.subscribe(vis.events.FILE_SAVE_PRE, function(file)
      file=']] .. file.path:gsub("'", "'\\''") .. [['
      owner="$(
        { ls -l "$file" || ls -l "$(dirname "$file")" || exit 1; } \
-         2>/dev/null | cut -d' ' -f4
+         2>/dev/null | awk '{ print $3 }'
      )"
      sudo env whoami="$(whoami)" owner="$owner" file="$file" sh -c '
        ! [ -e "$file" ] || touch "$file"
